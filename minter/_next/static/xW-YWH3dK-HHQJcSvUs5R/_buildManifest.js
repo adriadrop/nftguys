a97e6888fc2e119f80262418d785a28988431b7c
Marko <adriadrop@gmail.com>
2022-10-27 11:28:10 +0200

    update with different path

---
+++ minter/_next/static/xW-YWH3dK-HHQJcSvUs5R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-73a0bb3773c6cc58.js","static/chunks/pages/index-60b5d415ecc9c048.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
